e2fd0893f96fd602bbeaa92b430dbcef94b16e5d
Alan Silva <alanvncs.main@gmail.com>
2024-08-18 11:59:14 -0300

    another test

--- functions/index.ts
+++ functions/index.ts
@@ -1,10 +1,9 @@
 import proxy from './proxy/vtex';
 
 export const onRequest: PagesFunction<Env> = async ({ request, env }) => {
-	return new Response('ddddddddddddddddddd');
-	// const response = await proxy(request, env);
+	const response = await proxy(request, env);
 
-	// if (typeof response === 'undefined') return new Response('Erro ao montar a requisição');
+	if (typeof response === 'undefined') return new Response('Erro ao montar a requisição');
 
-	// return response;
+	return response;
 };
