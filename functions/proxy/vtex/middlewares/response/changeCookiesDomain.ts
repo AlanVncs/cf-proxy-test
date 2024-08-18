1d3c06935413a9b4be6e2c9bd5b0cc8d66cde3fb
Alan Silva <alanvncs.main@gmail.com>
2024-08-18 12:34:35 -0300

    new test

--- functions/proxy/vtex/middlewares/response/changeCookiesDomain.ts
+++ functions/proxy/vtex/middlewares/response/changeCookiesDomain.ts
@@ -1,28 +1,18 @@
-import { parse } from 'set-cookie-parser';
-import { serialize, CookieSerializeOptions } from 'cookie';
 import { ResponseMiddleware } from '../response';
 
 /**
  * @title Change cookies domain
  * @description Changes cookies domain to the same of requested url (normally 'localhost') */
-export const changeCookiesDomain: ResponseMiddleware = (request, _env, response) => {
+export const changeCookiesDomain: ResponseMiddleware = (request, env, response) => {
 	if (!response) return;
 
 	const { host: localDomain } = new URL(request.url);
+	const { host: targetDomain } = new URL(env.TARGET_ORIGIN);
 
-	const cookies = parse(response.headers.getSetCookie());
+	const strCookies = [...response.headers.getSetCookie()];
 	response.headers.delete('set-cookie');
 
-	for (const { name, value, ...remaining } of cookies) {
-		const cookieStr = serialize(name, value, { ...remaining, domain: localDomain } as CookieSerializeOptions);
-		response.headers.set('set-cookie', cookieStr);
+	for (const strCookie of strCookies) {
+		response.headers.set('set-cookie', strCookie.replaceAll(targetDomain, localDomain));
 	}
-
-	// console.log('----------------------------------------------------------------------------');
-	// console.log({ path: url.pathname });
-	// console.log({ cookies });
-	// console.log({ newCookies: parse(headers.getSetCookie()) });
-	// console.log('----------------------------------------------------------------------------');
-
-	// return { response: new Response };
 };
